723d743f45cf834856db98527aa539ff4316fc22
lotus77888 <lotus77888@gmail.com>
2017-08-27 02:41:47 +0530

    added default db postgrelsql in database file

--- config/database.php
+++ config/database.php
@@ -13,8 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
-
+    'default' => env('DB_CONNECTION', 'pgsql'),
     /*
     |--------------------------------------------------------------------------
     | Database Connections
